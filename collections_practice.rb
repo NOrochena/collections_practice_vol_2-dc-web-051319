7b9eb7f1b24ff8590c29869cfb86ff6fa0cea6bc
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-05-05 04:01:36 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,67 @@
 # your code goes here
+def begins_with_r(arr)
+  bool = true
+  arr.each do |val|
+    if !val.start_with?("r")
+      bool = false
+    end
+  end
+  bool
+end
+
+def contain_a(arr)
+  arr.keep_if do |val|
+    val.include? "a"
+  end
+end
+
+def first_wa(arr)
+  arr.each do |val|
+    if val.to_s.start_with?("wa")
+      return val
+    end
+  end
+end
+
+def remove_non_strings(arr)
+  arr.keep_if do |val|
+    val == val.to_s
+  end
+end
+
+def count_elements(arr)
+  count = []
+  counter = []
+  
+  arr.each_with_index do |val, index|
+    if count.include?(val)
+      counter[count.index(val)] += 1
+    else
+      counter.push(1)
+      count.push(val)
+    end
+  end
+  counter.each_with_index do |num, index|
+    count[index][:count] = num
+  end
+  count
+end
+
+def merge_data(keys, data)
+  arr = []
+  new_hash = {}
+  
+  keys.each do |obj|
+    arr.push(obj)
+  end
+  
+  data.each do |obj|
+    obj.each do |name, attributes|
+      puts arr.index("Blake")
+    end
+  end
+  
+  arr
+end
+
+
